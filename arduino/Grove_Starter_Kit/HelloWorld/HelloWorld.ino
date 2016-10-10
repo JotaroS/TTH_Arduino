9933969fa2aecfd56449bb4e6c267745e89a0fc7
KoyaNarumi <narumi@akg.t.u-tokyo.ac.jp>
2016-10-10 12:55:44 +0900

    Update HelloWorld.ino

--- arduino/Grove_Starter_Kit/HelloWorld/HelloWorld.ino
+++ arduino/Grove_Starter_Kit/HelloWorld/HelloWorld.ino
@@ -1,28 +1,60 @@
-// demo of Starter Kit V2.0 - Grove - Relay
-// when a button press, relay will on
+/*
+  Hello World.ino
+  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
 
-const int buttonPin = 3;     // the button is attached to digital pin 3
-const int relayPin =  9;     // the relay is attached to digital pin 9
-int buttonState = 0;
+  Author:Loovee
+  2013-9-18
 
-void setup()
+  Grove - Serial LCD RGB Backlight demo.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
+
+void setup() 
 {
-    pinMode(relayPin, OUTPUT);
-    pinMode(buttonPin, INPUT);
-}
-
-void loop()
-{
-    // read the state of the button:
-    buttonState = digitalRead(buttonPin);
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
     
-    if (buttonState == 1)   
-    {
-        digitalWrite(relayPin, HIGH);
-    }
-    else   
-    {
-        digitalWrite(relayPin, LOW);
-    }
-    delay(10);
+    lcd.setRGB(colorR, colorG, colorB);
+    
+    // Print a message to the LCD.
+    lcd.print("hello, world!");
+
+    delay(1000);
 }
+
+void loop() 
+{
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(millis()/1000);
+
+    delay(100);
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
